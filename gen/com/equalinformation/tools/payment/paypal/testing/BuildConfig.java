9ff1b17e5fb846200f0fd3d7d52dfa0953269d82
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2014-12-01 09:48:00 -0800

    Initial files, payment related android front end - back end combination, front-end project.

---
+++ gen/com/equalinformation/tools/payment/paypal/testing/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.equalinformation.tools.payment.paypal.testing;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
